c841530f90a9893cbb51f1cce897812a25a6de62
Connor Thompson <connorishonor@gmail.com>
2025-11-12 21:50:19 -0500

    Delete bookborrowing directory

--- bookborrowing/src/main/java/bookborrowing/auth/AuthController.java
+++
@@ -1,6 +0,0 @@
-package bookborrowing.auth;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-public class AuthController(){
-
-}
--- bookborrowing/src/main/java/bookborrowing/book/BookController.java
+++
@@ -1,6 +0,0 @@
-package bookborrowing.book;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-public class BookController(){
-
-}
