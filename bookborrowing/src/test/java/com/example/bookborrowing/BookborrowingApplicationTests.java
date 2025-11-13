8f70a3dbfb8d5e93935e07020df21430e613856b
conthom <conthom@iu.edu>
2025-11-12 20:37:40 -0500

    Spring skeleton project

---
+++ bookborrowing/src/test/java/com/example/bookborrowing/BookborrowingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookborrowing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookborrowingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
